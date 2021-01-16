ed5d7d2536f06f355196f1067452665335ccd680
Gabriel Menezes <gabrielufmscc@gmail.com>
2021-01-16 09:00:15 -0400

    Criando o DatabaseDataSource

---
+++ app/src/main/java/com/menezes/teammanager/repository/DatabaseDataSource.kt
@@ -0,0 +1,58 @@
+package com.menezes.teammanager.repository
+
+import com.menezes.teammanager.data.db.dao.PlayerDAO
+import com.menezes.teammanager.data.db.entity.PlayerEntity
+import java.util.*
+
+class DatabaseDataSource(private val playerDAO: PlayerDAO) : PlayerRepository {
+    override suspend fun insertPlayer(
+        name: String,
+        purchasePrice: Float,
+        purchaseDate: Date,
+        sellPrice: Float,
+        sellDate: Date
+    ): Long {
+        val player = PlayerEntity(
+            name = name,
+            purchasePrice = purchasePrice,
+            purchaceDate = purchaseDate,
+            sellPrice = sellPrice,
+            sellDate = sellDate
+        )
+        return playerDAO.insert(player)
+    }
+
+    override suspend fun updatePlayer(
+        id: Long,
+        name: String,
+        purchasePrice: Float,
+        purchaseDate: Date,
+        sellPrice: Float,
+        sellDate: Date
+    ) {
+
+        val player = PlayerEntity(
+            id = id,
+            name = name,
+            purchasePrice = purchasePrice,
+            purchaceDate = purchaseDate,
+            sellPrice = sellPrice,
+            sellDate = sellDate
+        )
+
+        playerDAO.update(player)
+
+    }
+
+    override suspend fun deletePlayer(id: Long) {
+        playerDAO.delete(id)
+    }
+
+    override suspend fun deleteAllPlayers() {
+        playerDAO.deleteAll()
+    }
+
+    override suspend fun getAllPlayers(): List<PlayerEntity> {
+        return playerDAO.getAll()
+    }
+}
