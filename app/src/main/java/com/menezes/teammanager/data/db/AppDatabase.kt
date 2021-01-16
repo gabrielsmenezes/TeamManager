bc518378787644d51c74ccbb886a3554c116e851
Gabriel Menezes <gabrielufmscc@gmail.com>
2021-01-16 08:35:55 -0400

    Criando o AppDatabase

---
+++ app/src/main/java/com/menezes/teammanager/data/db/AppDatabase.kt
@@ -0,0 +1,28 @@
+package com.menezes.teammanager.data.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.menezes.teammanager.data.db.dao.PlayerDAO
+import com.menezes.teammanager.data.db.entity.PlayerEntity
+
+@Database(entities = [PlayerEntity::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract val playerDAO: PlayerDAO
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            synchronized(this) {
+                var instance: AppDatabase? = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(context, AppDatabase::class.java, "app_database").build()
+                }
+                return instance
+            }
+        }
+    }
+}
